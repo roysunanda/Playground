a8ab4738db7adff37c4f6c19bcf5c5214ff75534
Sunanda Roy <roysunanda.update@gmail.com>
2026-09-05 23:19:27 +0530

    repo updated

--- WebDev/AnkitaK-NextJs16-Course/authy-project/auth.ts
+++ WebDev/AnkitaK-NextJs16-Course/authy-project/auth.ts
@@ -14,10 +14,11 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       // You can specify which fields should be submitted, by adding keys to the `credentials` object.
       // e.g. domain, username, password, 2FA token, etc.
       credentials: {
-        username: { label: "Username" },
-        password: { label: password, type: "password" },
+        username: { label: "Username", type: "text" },
+        password: { label: "password", type: "password" },
       },
       authorize: async (credentials) => {
+        console.log(credentials);
         return null;
       },
     }),
--- WebDev/AnkitaK-NextJs16-Course/authy-project/src/app/page.tsx
+++ WebDev/AnkitaK-NextJs16-Course/authy-project/src/app/page.tsx
@@ -3,8 +3,8 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <h1>hello world</h1>
-      <Button variant='outline'>Button</Button>
+      {/* <h1>hello world</h1> */}
+      <Button>Click me</Button>
     </div>
   );
 }
