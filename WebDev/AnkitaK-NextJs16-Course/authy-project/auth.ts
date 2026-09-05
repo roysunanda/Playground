557bee7f01bcb96c47062e079ab26eddad1c7223
Sunanda Roy <roysunanda.update@gmail.com>
2026-09-05 17:55:05 +0530

    creating credentials provider with next auth

--- WebDev/AnkitaK-NextJs16-Course/authy-project/auth.ts
+++ WebDev/AnkitaK-NextJs16-Course/authy-project/auth.ts
@@ -1,5 +1,25 @@
+// import NextAuth from "next-auth";
+
+// export const { handlers, signIn, signOut, auth } = NextAuth({
+//   providers: [],
+// });
+
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+// Your own logic for dealing with plaintext password strings; be careful!
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [
+    Credentials({
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        username: { label: "Username" },
+        password: { label: password, type: "password" },
+      },
+      authorize: async (credentials) => {
+        return null;
+      },
+    }),
+  ],
 });
