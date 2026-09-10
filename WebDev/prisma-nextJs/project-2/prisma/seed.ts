96b4ae575362090fe8f39111c0e6692eca9139d3
Sunanda Roy <roysunanda.update@gmail.com>
2026-09-10 23:32:11 +0530

    repo updated

--- WebDev/prisma-nextJs/project-2/prisma/seed.ts
+++ WebDev/prisma-nextJs/project-2/prisma/seed.ts
@@ -3,7 +3,7 @@ import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "../generated/prisma/client";
 
 const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
-const prisma = new PrismaClient({ adapter });
+export const prisma = new PrismaClient({ adapter });
 
 async function seed() {
   await prisma.user.createMany({
