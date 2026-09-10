7eb3fe36ae8adeb950a9b7624bdeb0ff69b7ad70
Sunanda Roy <roysunanda.update@gmail.com>
2026-09-10 17:18:13 +0530

    repo updated

---
+++ WebDev/prisma-nextJs/project-2/prisma/seed.ts
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "../generated/prisma/client";
+
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
+async function seed() {
+  await prisma.user.createMany({
+    data: [
+      { name: "Sunanda", email: "sunanda@email.com" },
+      { name: "Suparna", email: "suparna@email.com" },
+    ],
+  });
+}
+
+seed().then(() => {
+  prisma.$disconnect();
+});
