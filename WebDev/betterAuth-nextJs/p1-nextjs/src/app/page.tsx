2596dd2b0b7d2c74c968d10654b5d96a6dc2fc49
Sunanda Roy <roysunanda.update@gmail.com>
2026-09-12 16:23:35 +0530

    repo updated

--- WebDev/betterAuth-nextJs/p1-nextjs/src/app/page.tsx
+++ WebDev/betterAuth-nextJs/p1-nextjs/src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Auth } from "@better-auth-ui/heroui";
+
 export default function Home() {
-  return <h1>Homepage</h1>;
+  return (
+    <div>
+      <h1>Homepage</h1>
+      {/* <Auth view='signIn' /> */}
+    </div>
+  );
 }
