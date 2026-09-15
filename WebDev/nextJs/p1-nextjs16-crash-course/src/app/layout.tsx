18835cdaba02768f46947bf8dd72d678b80483c4
Sunanda Roy <roysunanda.update@gmail.com>
2026-09-15 23:59:10 +0530

    repo updated

--- WebDev/nextJs/p1-nextjs16-crash-course/src/app/layout.tsx
+++ WebDev/nextJs/p1-nextjs16-crash-course/src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html
       lang='en'
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className='min-h-full flex flex-col'>
+      <body className='min-h-full flex flex-col '>
         <Navbar />
         {children}
       </body>
