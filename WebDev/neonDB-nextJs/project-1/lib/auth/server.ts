c108b7e49028298ae0c9bd020f0582083af37558
Sunanda Roy <roysunanda.update@gmail.com>
2026-09-17 23:53:47 +0530

    repo updated

--- WebDev/neonDB-nextJs/project-1/lib/auth/server.ts
+++
@@ -1,9 +0,0 @@
-// ./lib/auth/server.ts
-import { createNeonAuth } from "@neondatabase/auth/next/server";
-// to use in react server components, server actions, and API routes
-export const auth = createNeonAuth({
-  baseUrl: process.env.NEON_AUTH_BASE_URL!,
-  cookies: {
-    secret: process.env.NEON_AUTH_COOKIE_SECRET!,
-  },
-});
--- WebDev/neonDB-nextJs/project-1/src/app/api/auth/[...path]/route.ts
+++
@@ -1,5 +0,0 @@
-// import { auth } from "@/lib/auth/server";
-
-import { auth } from "../../../../../lib/auth/server";
-
-export const { GET, POST } = auth.handler();
